fb20728ec07e643b5b2e38199ff3529ba0be3c39
shura <shura1991@gmail.com>
2012-10-30 20:58:59 +0100

    Fix endianness things int shurizzle.witrans.net.subnet.IPv4Subnet

--- src/shurizzle/witrans/net/subnet/IPv4Subnet.java
+++ src/shurizzle/witrans/net/subnet/IPv4Subnet.java
@@ -4,28 +4,36 @@ import java.lang.UnsupportedOperationException;
 import java.lang.IllegalStateException;
 import java.util.NoSuchElementException;
 import java.net.InetAddress;
+import java.nio.ByteOrder;
 
 import shurizzle.witrans.net.Subnet;
 
 public class IPv4Subnet extends Subnet
 {
-  private int network;
-  private int netmask;
-  private int broadcast;
+  private static final int[] idx = (ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN) ? new int[]{0,1,2,3} : new int[]{3,2,1,0};
+  private final byte[] network;
+  private final InetAddress networkAddress;
+  private final byte[] netmask;
+  private final InetAddress netmaskAddress;
+  private final short prefix;
+  private final byte[] broadcast;
+  private final InetAddress broadcastAddress;
 
   public class Iterator implements java.util.Iterator<InetAddress>
   {
-    private int current;
+    private byte[] current;
+    private final byte[] max;
 
     public Iterator()
     {
-      current = network + 1;
+      current = IPv4Subnet.next(network);
+      max = prev(broadcast);
     }
 
     @Override
     public boolean hasNext()
     {
-      return (current + 1) < broadcast;
+      return lt(current, max);
     }
 
     @Override
@@ -33,7 +41,10 @@ public class IPv4Subnet extends Subnet
       throws NoSuchElementException {
       if (!hasNext())
         throw new NoSuchElementException();
-      return toInetAddress(current++);
+
+      InetAddress res = toInetAddress(current);
+      current = IPv4Subnet.next(current);
+      return res;
     }
 
     @Override
@@ -45,32 +56,35 @@ public class IPv4Subnet extends Subnet
 
   private IPv4Subnet(int address, short netmask)
   {
-    this.netmask = 0;
-    for (int i = 0; i < netmask; i++) {
-      this.netmask |= (1 << 31 - i);
-    }
-    network = address & this.netmask;
-    broadcast = address | ~(this.netmask);
+    int nm = netmaskShortToInteger(netmask);
+
+    prefix = (short) countBits(nm);
+    this.netmask = toArray(nm);
+    netmaskAddress = toInetAddress(this.netmask);
+    network = toArray(address & nm);
+    networkAddress = toInetAddress(network);
+    broadcast = toArray(address | ~(nm));
+    broadcastAddress = toInetAddress(broadcast);
   }
 
   public InetAddress getNetwork()
   {
-    return toInetAddress(network);
+    return networkAddress;
   }
 
   public InetAddress getNetmask()
   {
-    return toInetAddress(netmask);
+    return netmaskAddress;
   }
 
   public short getPrefix()
   {
-    return (short) countBits(netmask);
+    return prefix;
   }
 
   public InetAddress getBroadcast()
   {
-    return toInetAddress(broadcast);
+    return broadcastAddress;
   }
 
   public Iterator iterator()
@@ -81,13 +95,53 @@ public class IPv4Subnet extends Subnet
   @Override
   public String toString()
   {
-    return String.format("%s/%d", getNetwork().getHostAddress(), countBits(netmask));
+    return String.format("%s/%d", getNetwork().getHostAddress(), (int) getPrefix());
   }
 
-  public static InetAddress toInetAddress(int x)
+  public static byte[] next(byte[] addr)
+  {
+    int i = addr.length - 1;
+    for (; i >= 0 && addr[i] == (byte) 0xFF; addr[i] = 0, i--);
+    if (i >= 0)
+      addr[i]++;
+
+    return addr;
+  }
+
+  public static byte[] prev(byte[] addr)
+  {
+    int i = addr.length - 1;
+    for (; i >= 0 && addr[i] == (byte)0x00; addr[i] = (byte) 0xFE, i--);
+    if (i >= 0)
+      addr[i]--;
+
+    return addr;
+  }
+
+  public static boolean lt(byte[] addr1, byte[] addr2)
+  {
+    if (addr1.length > addr2.length)
+      return false;
+    else if (addr2.length > addr1.length)
+      return true;
+
+    int max = addr1.length - 1;
+    for (int i = 0; i < max; i++) {
+      int a1 = addr1[i] & 0xFF,
+          a2 = addr2[i] & 0xFF;
+      if (a1 > a2)
+        return false;
+      else if (a2 > a1)
+        return true;
+    }
+
+    return addr1[max] < addr2[max];
+  }
+
+  public static InetAddress toInetAddress(byte[] x)
   {
     try {
-      return InetAddress.getByAddress(toArray(x));
+      return InetAddress.getByAddress(x);
     } catch (Exception e) {
       return null;
     }
@@ -102,58 +156,75 @@ public class IPv4Subnet extends Subnet
     return bits;
   }
 
+  public static int netmaskShortToInteger(short netmask)
+  {
+    int nm = 0;
+
+    if (ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN) {
+      for (int i = 0; i < netmask; i++) {
+        int id = 31 - i;
+        nm |= 1 << (Math.abs((int) (id / 8) - 3) * 8 + id % 8);
+      }
+    } else {
+      for (int i = 0; i < netmask; i++) {
+        nm |= (1 << 31 - i);
+      }
+    }
+
+    return nm;
+  }
+
   public static byte[] toArray(int x)
   {
     byte[] ret = new byte[4];
-    for (int j = 3; j >= 0; --j)
-        ret[j] |= ((x >>> 8 * (3 - j)) & (0xff));
+
+    ret[idx[0]] = (byte) (x & 0xFF);
+    ret[idx[1]] = (byte) (0xFF & x >>> 8);
+    ret[idx[2]] = (byte) (0xFF & x >>> 16);
+    ret[idx[3]] = (byte) (0xFF & x >>> 24);
+
     return ret;
   }
 
-  private static int parseIp(String ip)
+  public static int toInteger(byte[] address)
   {
-    int addr = 0;
-    String[] ps = ip.split("\\.");
-
-    for (int i = 0; i < ps.length; i++) {
-      addr = (addr << 8) + Integer.parseInt(ps[i]);
-    }
-
-    return addr;
+    return ((address[idx[3]] & 0xFF) << 24) |
+      ((address[idx[2]] & 0xFF) << 16) |
+      ((address[idx[1]] & 0xFF) << 8) |
+      (address[idx[0]] & 0xFF);
   }
 
-  public static int pop(int x)
-  {
-    x = x - ((x >>> 1) & 0x55555555);
-    x = (x & 0x33333333) + ((x >>> 2) & 0x33333333);
-    x = (x + (x >>> 4)) & 0x0F0F0F0F;
-    x = x + (x >>> 8);
-    x = x + (x >>> 16);
-    return x & 0x0000003F;
-  }
-
-  public static short netmaskDotToShort(String mask)
-  {
-    return (short) pop(parseIp(mask));
+  public static int parseIp(String ip)
+    throws java.net.UnknownHostException {
+    return toInteger(InetAddress.getByName(ip).getAddress());
   }
 
   public static IPv4Subnet fromIpNetmask(String addr, String netmask)
   {
-    return fromIpNetmask(addr, netmaskDotToShort(netmask));
+    try {
+      return fromIpNetmask(addr, (short) countBits(parseIp(netmask)));
+    } catch (Exception e) {
+      return null;
+    }
   }
 
   public static IPv4Subnet fromIpNetmask(String addr, short netmask)
   {
     if (netmask < 0 || netmask > 31)
       return null;
-    if (!isIPv4(addr))
-      return null;
 
-    return new IPv4Subnet(parseIp(addr), netmask);
+    try {
+      return new IPv4Subnet(parseIp(addr), netmask);
+    } catch (Exception e) {
+      return null;
+    }
   }
 
   public static IPv4Subnet fromInetAddress(InetAddress addr, short netmask)
   {
+    if (netmask < 0 || netmask > 31)
+      return null;
+
     return fromIpNetmask(addr.getHostAddress(), netmask);
   }
 
