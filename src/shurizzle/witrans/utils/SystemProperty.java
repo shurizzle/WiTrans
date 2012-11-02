c5a1b0ce0e15cce94910c4727633bcab38ec6214
shura <shura1991@gmail.com>
2012-11-02 18:27:24 +0100

    improve shurizzle.witrans.utils.SystemProperty, now using reflection in place of calling system bin

--- src/shurizzle/witrans/utils/SystemProperty.java
+++ src/shurizzle/witrans/utils/SystemProperty.java
@@ -1,47 +1,34 @@
 package shurizzle.witrans.utils;
 
-import java.lang.StringBuilder;
-import java.io.InputStreamReader;
-import java.lang.Process;
-import java.lang.Runtime;
+import java.lang.ClassLoader;
+import java.lang.Class;
+import java.lang.reflect.Method;
 
 public class SystemProperty
 {
-  private SystemProperty() {}
+  private static Class SYSPROP;
+  private static Method GET_METHOD;
 
-  private static String readAll(InputStreamReader read)
-  {
-    StringBuilder sb = new StringBuilder();
-
+  static {
+    ClassLoader classLoader = SystemProperty.class.getClassLoader();
     try {
-      char[] cbuf = new char[1024];
-      while (read.ready()) {
-        int r = read.read(cbuf, 0, 1024);
-        sb.append(cbuf, 0, r);
-      }
+      SYSPROP = classLoader.loadClass("android.os.SystemProperties");
+      GET_METHOD = SYSPROP.getMethod("get", String.class, String.class);
     } catch (Exception e) {}
-
-    return sb.toString();
   }
+
+  private SystemProperty() {}
 
   public static String get(String key, String def)
   {
+    String res;
+
     try {
-      Process proc = Runtime.getRuntime().exec(new String[] {"getprop", key});
-      proc.waitFor();
-
-      String res = readAll(new InputStreamReader(proc.getInputStream()));
-      if (res.charAt(res.length() - 1) == '\n') {
-        if (res.charAt(res.length() - 2) == '\r') {
-          res = res.substring(0, res.length() - 2);
-        } else {
-          res = res.substring(0, res.length() - 1);
-        }
-      }
-
-      return res;
+      res = (String) GET_METHOD.invoke(SYSPROP, key, def);
     } catch (Exception e) {
-      return null;
+      res = def;
     }
+
+    return res;
   }
 }
